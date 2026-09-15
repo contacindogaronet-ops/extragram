a9cbb1c0e24fa74595a22cf468c6ba56072e8956
contacindogaronet-ops <contacindogaro.net@gmail.com>
2026-09-15 22:48:06 +0700

    tes5

--- TMessagesProj/src/main/java/org/telegram/ui/DevOpsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/DevOpsActivity.java
@@ -38,8 +38,7 @@ public class DevOpsActivity extends BaseFragment {
         RecyclerListView listView = new RecyclerListView(context);
         listView.setLayoutManager(new androidx.recyclerview.widget.LinearLayoutManager(context, androidx.recyclerview.widget.LinearLayoutManager.VERTICAL, false));
 
-        // Menggunakan Lambda eksplisit untuk menghindari mismatch signature
-        adapter = new UniversalAdapter(listView, context, currentAccount, 0, this::fillItems, (item, view) -> onRowClick(item, view));
+        adapter = new UniversalAdapter(listView, context, currentAccount, 0, this::fillItems, this::onRowClick);
         listView.setAdapter(adapter);
 
         fragmentView = listView;
@@ -66,7 +65,7 @@ public class DevOpsActivity extends BaseFragment {
         items.add(UItem.asButton(208, "Build Commit Hash", BuildVars.BUILD_GIT_HASH));
     }
 
-    private void onRowClick(UItem item, View view) {
+    private void onRowClick(UItem item, View view, int position, float x, float y) {
         if (item == null) return;
 
         switch (item.id) {
