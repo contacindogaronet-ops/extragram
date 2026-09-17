d3eb2a2ca4f0ea1b099c1dcb71150fb18732d63c
contacindogaronet-ops <contacindogaro.net@gmail.com>
2026-09-18 01:34:38 +0700

    cek7

--- TMessagesProj/src/main/java/org/telegram/ui/DevOpsOtaActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/DevOpsOtaActivity.java
@@ -124,7 +124,7 @@ public class DevOpsOtaActivity extends BaseFragment {
         frameLayout.addView(listView, LayoutHelper.createFrame(LayoutHelper.MATCH_PARENT, LayoutHelper.MATCH_PARENT));
 
         return fragmentView;
-       }
+    }
     // --- ADAPTER BARU (Ganti seluruh class ListAdapter lama dengan ini) ---
     private class ListAdapter extends RecyclerListView.SelectionAdapter {
         private final Context mContext;
@@ -177,9 +177,6 @@ public class DevOpsOtaActivity extends BaseFragment {
                 });
             }
         }
-    }
-
-        return fragmentView;
     }
 
     private void checkForGithubRelease(boolean showToast) {
